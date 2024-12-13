592feebe8cdcde2a491276430d54605a9f3643d0
Ahamed Fofana <ahamedfofana12@gmail.com>
2024-12-13 10:25:55 -0500

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -9,6 +9,10 @@
 #include <netdb.h>
 #include <vector>
 #include <sstream>
+#include <thread>
+  
+  
+
 
 bool startsWith(const std::string& str, const std::string& prefix) {
     return str.compare(0, prefix.size(), prefix) == 0;
@@ -24,6 +28,35 @@ std::vector<std::string> split_message_by_line(const std::string& message){
   }
   return tokens;
 } 
+
+void new_client(int client){
+    std::string input_buffer(1024, '\0');
+    recv(client, (void *)&input_buffer[0],input_buffer.max_size(), 0);
+    std::string input = std::string(input_buffer);
+    std::string output_message = ""; 
+    std::vector<std::string> parts = split_message_by_line(input_buffer);
+    bool is_home_page = startsWith(std::string(input_buffer), "GET / HTTP/1.1");
+    bool is_echo_string = startsWith(std::string(input_buffer), "GET /echo");
+    bool is_user_agent = startsWith(std::string(input_buffer), "GET /user-agent");
+
+    if(is_home_page) {
+      output_message = "HTTP/1.1 200 OK\r\n\r\n";
+    } else if(is_echo_string){
+      int echo_cmd_len = 10;
+      std::string full_str_no_cmd = input_buffer.substr(echo_cmd_len);
+      int end_of_echo = full_str_no_cmd.find(' ');
+      std::string echo = full_str_no_cmd.substr(0, end_of_echo);
+
+      output_message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(echo.length()) + "\r\n\r\n" + echo;
+    } else if (is_user_agent){
+      std::string user_agent_content = parts[2].substr(parts[2].find(':') + 2);
+      output_message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(user_agent_content.length() - 1) + "\r\n\r\n" + user_agent_content;
+      std::cout << output_message;
+    }else {
+      output_message = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+    send(client, output_message.c_str(), output_message.length(), 0);
+}
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -62,45 +95,18 @@ int main(int argc, char **argv) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  std::cout << "Waiting for a client to connect...\n";
-  
-  int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
-  // std::cout << "argv is: " << argv[0] << " argc is: " << argc;
-  // the int response in recv tells me how many bytes were returned, -1 means error, 0 means connections closed
-  std::string input_buffer(1024, '\0');
-  recv(client, (void *)&input_buffer[0],input_buffer.max_size(), 0);
-  std::string input = std::string(input_buffer);
-  std::string output_message = ""; 
-  std::vector<std::string> parts = split_message_by_line(input_buffer);
-  bool is_home_page = startsWith(std::string(input_buffer), "GET / HTTP/1.1");
-  bool is_echo_string = startsWith(std::string(input_buffer), "GET /echo");
-  bool is_user_agent = startsWith(std::string(input_buffer), "GET /user-agent");
-  if(is_home_page) {
-    output_message = "HTTP/1.1 200 OK\r\n\r\n";
-  } else if(is_echo_string){
-    int echo_cmd_len = 10;
-    std::string full_str_no_cmd = input_buffer.substr(echo_cmd_len);
-    int end_of_echo = full_str_no_cmd.find(' ');
-    std::string echo = full_str_no_cmd.substr(0, end_of_echo);
-
-    output_message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(echo.length()) + "\r\n\r\n" + echo;
-  } else if (is_user_agent){
-    //4th index is the reference i need
-    std::string user_agent_content = parts[2].substr(parts[2].find(':') + 2);
-    output_message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(user_agent_content.length() - 1) + "\r\n\r\n" + user_agent_content;
-    // std::cout << "This Is Part 2 " << parts[2];
-    std::cout << output_message;
-  }else {
-    output_message = "HTTP/1.1 404 Not Found\r\n\r\n";
+  while(true) {
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    
+    std::cout << "Waiting for a client to connect...\n";
+    
+    int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    std::cout << "Client connected\n";
+    std::thread th(new_client, client);
+    th.detach();
+    //add a copy of client from memroy into another datastructure so it's okayy if it is written over
   }
-
-  send(client, output_message.c_str(), output_message.length(), 0);
   close(server_fd);
-
   return 0;
 }
